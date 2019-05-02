768e09a1238594bc6239048f4666b5620da091bc
Yohei Shiina <yohei_shiina@YSMBA.local>
2019-05-02 18:18:14 +0900

    bootstrap4の導入

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,15 +2,20 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+
         <title>GROWER</title>
     </head>
     <body>
             @if (Auth::check())
-            <a href="logout">ログアウト</a>
+            <a href="logout" class="hoge">ログアウト</a>
             @else
             <a href="login">ログイン</a>
             <a href="register">新規登録</a>
             @endif
         @yield('content')
+
+        <script src="./js/jquery-3.3.1.min.js"></script>
+        <script src="./js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
