74623540a82047df1b97eec9073be585130e88bc
Yohei Shiina <yohei_shiina@YSMBA.local>
2019-05-07 16:24:06 +0900

    誤削除したファイルをチェックアウトし復元するためのコミット

--- resources/views/targets/show.blade.php
+++ resources/views/targets/show.blade.php
@@ -1,27 +1,50 @@
 @extends('layout')
 
 @section('content')
-<h1>目標</h1>
-<div> {{$target->goal}} </div> 
-<a href="/targets/{{$target->id}}/edit">目標の編集</a>
-<a href="/targets/{{$target->id}}/delete">目標の削除</a>
-<h1>期限</h1>
-<div>{{ $target->due()->days . "日"}}</div>
 
-<h1>タスク</h1>
-<ol>
-  @foreach($tasks as $task)
-  <li>{{$task->task}}</li>
-  <form method="post" action="/targets/{{$target->id}}/tasks/{{$task->id}}">
-    {{ csrf_field() }}
-    <button type="submit">削除</button>
-  </form>
-  @endforeach
-</ol>
-{{ Form::open(['url' => "/targets/{$target->id}/tasks", 'method' => 'post']) }}
-{{ Form::text('task', null, ["placeholder" => 'タスクを入力']) }}
-{{ Form::submit('send') }}
-{{ Form::close()}}
+<h4 class="page">プロジェクト</h4>
+<div class="row"> 
+  <div class="title col-lg-10 col-md-12">
+    <h4 class="row">目標
+      <div class="btns">
+        <a href="/targets/{{$target->id}}/edit" class="btn btn-success">編集</a>
+        <a href="/targets/{{$target->id}}/delete" class="btn btn-danger">削除</a>
+      </div>
+    </h4>
+    <div class="goal breadcrumb bg-white"> {{$target->goal}} </div>
+  </div>
 
+  <div class="due col-lg-2 col-md-12">
+    <h6>期限</h6>
+    <span class="due-time list-group-item rounded">{{ $target->due()->days . "日"}}</span>
+    <h6>経過時間</h6>
+    <span class=" passed-time list-group-item rounded">15時間23分</span>
+  </div>
+  
+  <div class="task col-md-12">
+    <h4>タスク</h4>
+    <div class="tasks">
+    <?php $i = 1; ?>
+    @foreach($tasks as $task)
+    <div class="list breadcrumb bg-white">
+      <span>{{ $i }}. {{$task->task}}</span>
+      <form method="post" action="/targets/{{$target->id}}/tasks/{{$task->id}}">
+        {{ csrf_field() }}
+        <button type="submit" class="btn btn-warning">完了</button>
+        <button type="submit" class="btn btn-danger">削除</button>
+      </form>
+    </div>
+    <?php $i++ ?>
+    @endforeach
+    </div>
+  </div>
+  <div class="form-group">
+    {{ Form::open(['url' => "/targets/{$target->id}/tasks", 'method' => 'post', 'class' => 'form']) }}
+      <div class="row shadow">
+    {{ Form::text('task', null, ["placeholder" => 'タスクを入力', 'class' => 'input']) }}
+    {{ Form::submit('追加', ['class' => "btn btn-primary"]) }}
+    </div>{{ Form::close()}}
+  </div>
+</div>
 
 @endsection
--- resources/views/user-layout.blade.php
+++ resources/views/user-layout.blade.php
@@ -3,18 +3,13 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" href="./css/bootstrap.min.css">
+        <link rel="stylesheet" href="./css/user-layout.css">
 
         <title>GROWER</title>
     </head>
     <body>
-            @if (Auth::check())
-            <a href="logout" class="hoge">ログアウト</a>
-            @else
-            <a href="login">ログイン</a>
-            <a href="register">新規登録</a>
-            @endif
         @yield('content')
-
+        
         <script src="./js/jquery-3.3.1.min.js"></script>
         <script src="./js/bootstrap.bundle.min.js"></script>
     </body>
