0efe847c1ed1f335fe5d6d3f2852ae6983fea124
Yohei Shiina <yohei_shiina@YSMBA.local>
2019-06-21 12:46:49 +0900

    画像アップロード後のバグ修正

--- app/Http/Controllers/AvatarController.php
+++ app/Http/Controllers/AvatarController.php
@@ -1,31 +1,41 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Avatar;
 use Auth;
 use Illuminate\Support\Facades\Storage;
 
 class AvatarController extends Controller
 {
     public function upload(Request $request) {
-        if($request->file('image') === null) {
+        if($request->file('image') === null){
             return back();
         }
         $file = $request->file('image');
         $user = Auth::user();
         $disk = Storage::disk('s3');
-        // S３に保存
-        $path = $disk->putFile("/", $file, "public");
-
-        // 設定画像があればS3から削除する
-        // 削除じゃなくて上書きできそう　リファクタリング余地あり
-        if ($user->avatar) {
+        
+        // まだ画像がなければ
+        if ($user->avatar == null){
+            // S３に保存
+            $path = $disk->putFile("/", $file, "public");
+            // DBに保存
+            Avatar::create([
+                "image" => $path,
+                "user_id" => $user->id,
+            ]);
+        // 画像があれば
+        }else{
+            // S3から削除
             $disk->delete($user->avatar->image);
+            // S３に保存
+            $path= $disk->putFile("/", $file, "public");
+            // DBを更新
+            $user->avatar->update([
+                "image" => $path,
+            ]);
         }
-        $user->avatar->update([
-            "image" => $path,
-        ]);
-        back();
+        return back();
     }
 }
